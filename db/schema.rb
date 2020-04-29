088828455fca867a87de3af6e802d198f78943ef
gcp632dsh <gcp632dsh@gmail.com>
2020-04-29 23:25:57 +0900

    ページネーション機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_134038) do
+ActiveRecord::Schema.define(version: 2020_04_29_122824) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_134038) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.integer "likes_count", default: 0, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
