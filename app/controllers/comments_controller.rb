bc0df86dd82f0536d1c4c804e306ff17423ee762
gcp632dsh <gcp632dsh@gmail.com>
2020-04-29 12:00:14 +0900

    コメント機能(Ajax)の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,15 @@
 class CommentsController < ApplicationController
-    def create
-        @comment = Comment.new(comment_params)
-        @comment.user_id = current_user.id
-        if @comment.save
-          redirect_back(fallback_location: root_path)
-        else
-          redirect_back(fallback_location: root_path)
-        end
+  before_action :authenticate_user!
+  
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      render :index
     end
+  end
     
-      private
-      def comment_params
-        params.require(:comment).permit(:content)
-      end
+  private
+    def comment_params
+      params.require(:comment).permit(:content, :post_id)
+    end
 end
