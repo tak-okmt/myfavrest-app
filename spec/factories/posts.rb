c408bd49d56d0d0d890253ef91c60d6aa82f852f
gcp632dsh <gcp632dsh@gmail.com>
2020-05-17 17:55:43 +0900

    add model spec, factories and create request spec

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    
+   sequence(:title) { |n| "テスト投稿#{n}"}
+   description {"口コミのテストです"}
+   association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
-    factory :user do
-      user_id { "taro1" }
-      email { "test1@gmail.com" }
-      password { "testpassword" }
-    end
+  factory :user do
+    sequence(:user_id) { |n| "taro#{n}" }
+    sequence(:email) { |n| "test#{n}@gmail.com" }
+    password { "testpassword" }
   end
-  
+end
