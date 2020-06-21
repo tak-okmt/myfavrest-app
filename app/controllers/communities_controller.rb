3389f3dc7c554faa9de67551f626ce3d90b255dd
gcp632dsh <gcp632dsh@gmail.com>
2020-06-21 23:40:16 +0900

    create Community new and edit screen and modify user show

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -9,8 +9,14 @@ class CommunitiesController < ApplicationController
 
     def show
         @community = Community.find(params[:id])
+        @belongings = Belonging.where(community_id: @community.id)
         @posts = Post.where(community_id: @community.id)
         @posts = @posts.page(params[:page])
+    end
+
+    def new
+        @community = Community.new
+        @code = Code.all
     end
 
     def edit
@@ -18,10 +24,20 @@ class CommunitiesController < ApplicationController
         @code = Code.all
     end
 
+    def create
+        @community = current_user.communities.build(community_params)
+        
+        if @community.save
+          redirect_to communities_url, notice:"投稿「#{@community.name}」を登録しました。"
+        else
+          render :new
+        end
+    end
+
     def update
         community = current_user.communities.find(params[:id])
         community.update!(community_params)
-        redirect_to community_url(community) notice:"投稿「#{community.name}」を更新しました。"
+        redirect_to community_url(community), notice:"投稿「#{community.name}」を更新しました。"
     end
 
     def destroy
@@ -33,7 +49,7 @@ class CommunitiesController < ApplicationController
     private
 
         def community_params
-            params.require(:community).permit(:name,:create_user_id,:publish_flg)
+            params.require(:community).permit(:name,:create_user_id,:publish_flg, :description)
         end
 
 end
