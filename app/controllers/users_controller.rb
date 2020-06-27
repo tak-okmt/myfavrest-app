bebe0d2cd583aff0149ad0bb91e721e71773caf8
gcp632dsh <gcp632dsh@gmail.com>
2020-06-27 23:32:43 +0900

    create myuser menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,24 @@ class UsersController < ApplicationController
 
     def show
       @user = User.find(params[:id])
-      @comments = @user.comments.order('updated_at DESC')
-      @belongings = @user.belongings
+      if @user == current_user
+        # 選択したユーザがログインユーザの場合、自ユーザ画面へ遷移
+        @belongings = @user.belongings
+        @tab = params[:tab_id]
+        @community = Community.find(1) # 誰でもコミュニティ
+        if params[:tab_id]
+          respond_to do |format|
+            format.json { render "users/mycom" }  if @tab = '1'
+            format.json { render "users/pubcom" } if @tab = '2'
+          end
+        else
+          render template: "users/myusershow"
+        end
+      else
+        # ログインユーザ以外のユーザの場合、ユーザ詳細画面へ遷移
+        @comments = @user.comments.order('updated_at DESC')
+        @belongings = @user.belongings
+      end
     end
 
 end
