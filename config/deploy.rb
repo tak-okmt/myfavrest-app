fb07eaf86e1a15fbe2f8189dfa3855f8ad8ef395
gcp632dsh <gcp632dsh@gmail.com>
2020-07-12 17:20:46 +0900

    introduce capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,32 +2,31 @@
 lock "3.7.0"
 
 set :application, "myrest-app"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:gcp632dsh/myfavrest-app.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# deploy先のディレクトリ。 
+set :deploy_to, '/var/myapp'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+namespace :deploy do
+    desc "Make sure local git is in sync with remote."
+    task :confirm do
+      on roles(:app) do
+        puts "This stage is '#{fetch(:stage)}'. Deploying branch is '#{fetch(:branch)}'."
+        puts 'Are you sure? [y/n]'
+        ask :answer, 'n'
+        if fetch(:answer) != 'y'
+          puts 'deploy stopped'
+          exit
+        end
+      end
+    end
+  
+    desc 'Initial Deploy'
+    task :initial do
+      on roles(:app) do
+        invoke 'deploy'
+      end
+    end
+  
+    before :starting, :confirm
+  end
