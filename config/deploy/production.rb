9fa3af21f092bc88359b8a54eb5de95830c14c8a
gcp632dsh <gcp632dsh@gmail.com>
2020-07-19 22:54:57 +0900

    modify capistrano files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '52.203.94.78', user: 'ec2-user', roles: %w{app db web}, ssh_options: {
+server '54.226.182.2', user: 'ec2-user', roles: %w{app db web}, ssh_options: {
     # capistranoコマンド実行者の秘密鍵
     keys: [File.expand_path('~/.ssh/myportfolio-ssh-key.pem')],
     forward_agent: true,
