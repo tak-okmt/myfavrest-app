a35b8516fe72d277731bc95fda6a72b48a2062ee
gcp632dsh <gcp632dsh@gmail.com>
2020-12-02 20:29:09 +0900

    modify deploy.rb file by rubobop

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,40 +26,40 @@ set :keep_releases, 3
 
 # ----------カスタマイズしたタスク------------
 namespace :deploy do
-    # linked_filesで使用するファイルをアップロードするタスク。deploy前に実行する。
-    desc 'upload linked_files'
-    task :upload do
-        on roles(:app) do |_host|
-            execute :mkdir, '-p', "#{shared_path}/config"
-            upload!('config/database.yml', "#{shared_path}/config/database.yml")
-            upload!('config/master.key', "#{shared_path}/config/master.key")
+  # linked_filesで使用するファイルをアップロードするタスク。deploy前に実行する。
+  desc 'upload linked_files'
+  task :upload do
+    on roles(:app) do |_host|
+      execute :mkdir, '-p', "#{shared_path}/config"
+      upload!('config/database.yml', "#{shared_path}/config/database.yml")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
 
-            # puma.rbをデプロイ時に毎回作成する
-            invoke 'puma:config'
-        end
+      # puma.rbをデプロイ時に毎回作成する
+      invoke 'puma:config'
     end
+  end
 end
 
 # nginxの起動・停止・再起動
 namespace :nginx do
-    %w[start stop restart].each do |command|
-        desc "#{command} nginx"
-        task command.to_s do
-            on roles(:web) do
-                sudo "service nginx #{command}"
-            end
-        end
+  %w[start stop restart].each do |command|
+    desc "#{command} nginx"
+    task command.to_s do
+      on roles(:web) do
+        sudo "service nginx #{command}"
+      end
     end
+  end
 end
 
 # capistrano内の変数一覧表示
 namespace :config do
-    desc 'show variables'
-    task :display do
-        Capistrano::Configuration.env.keys.each do |key|
-        puts "#{key} => #{fetch(key)}"
-        end
+  desc 'show variables'
+  task :display do
+    Capistrano::Configuration.env.keys.each do |key|
+      puts "#{key} => #{fetch(key)}"
     end
+  end
 end
 
 # デプロイ開始前のサーバ停止タスク(nginx => puma)
