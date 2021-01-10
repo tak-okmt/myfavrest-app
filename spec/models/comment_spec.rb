9de4d8b9a5cbd72db62ca3f9bade8aed1a3aef38
gcp632dsh <gcp632dsh@gmail.com>
2021-01-10 20:30:58 +0900

    add comment model spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:community) { FactoryBot.create(:community) }
+  let(:post) { FactoryBot.create(:post, user: user, community: community)}
+
+  before do
+    @valid_comment = FactoryBot.create(:comment, user_id: user.id, post_id: post.id)
+  end
+
+  # 存在性チェック
+  describe "test of presence" do
+    # 内容、評価、訪問日があれば有効な状態であること
+    it "is valid with content, score and visitday" do
+      expect(@valid_comment).to be_valid
+    end
+
+  end
+
+  # # 一意性チェック
+  # describe "test of uniqueness" do
+
+  # end
+
+  # # 長さの確認
+  # describe "test of length" do
+
+  # end
+
+
+  # # 画像のアップロード
+  # describe "check image upload" do
+
+  # end
+
+
+  # # 削除の依存関係
+  # describe "dependent: destoy" do
+
+  # end
+
+end
