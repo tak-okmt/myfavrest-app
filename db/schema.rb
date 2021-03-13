7be25fd78564742d412c23defb7a33ca3629bfe9
tak-okmt <gcp632dsh@gmail.com>
2021-03-13 19:46:26 +0900

    リファクタリング

--- db/schema.rb
+++ db/schema.rb
@@ -128,10 +128,12 @@ ActiveRecord::Schema.define(version: 2020_12_10_135611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "user_id"
     t.date "birth_ym"
     t.string "work_idt"
     t.string "work_ocpn"
     t.string "gender"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
